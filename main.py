05139f7508ee359a17a8af3c0d1f4d67169a57d6
arul.thileeban <arulthileeban023@gmail.com>
2020-07-17 17:44:48 -0400

    Added Hash comparison mechanism

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ def hello():
 
 @app.route('/file',methods=['GET','POST'])
 def file():
+    shasher=hashlib.sha1()
+    mhasher=hashlib.md5()
     if request.method=="POST":
         ufile=request.files['ufile']
-        curr='test'
-        while curr!=b'':
-            curr=ufile.read(1024)
-            hasher.update(curr)
-        return hasher.hexdigest()
+        hash=request.form['hash']
+        og_hash=request.form['trhash']
+        for line in iter(ufile):
+            if hash=="sha1":
+                shasher.update(line)
+            elif hash=="md5":
+                mhasher.update(line)
+        final_hash=shasher.hexdigest() if hash=="sha1" else mhasher.hexdigest()
+        if final_hash==og_hash:
+            return "File is incorrupted. Calculated hash is: "+final_hash
+        else:
+            return "File is corrupted. Calculated hash is: "+final_hash
     return render_template('file.html')
 
 if __name__ == '__main__':
-    hasher=hashlib.sha1()
     app.run()
